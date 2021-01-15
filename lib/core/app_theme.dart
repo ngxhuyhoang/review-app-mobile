7c93dd65eeaf0563b3ca5e927ee49e3951d3ec34
Nguyen Huy Hoang <hoangnh@ahiho.com>
2021-01-15 21:48:58 +0700

    feat: test

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+// Color palette: https://flatuicolors.com/palette/cn
+
+class AppTheme {
+  static final Color black = Colors.black;
+  static final Color white = Colors.white;
+  static final Color blue1 = Color(0xff3742fa);
+  static final Color blue2 = Color(0xff5352ed);
+  static final Color blue3 = Color(0xff1e90ff);
+  static final Color blue4 = Color(0xff70a1ff);
+}
---
+++ lib/navigators/tab_bar_navigator.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:review_app_mobile/core/app_theme.dart';
+import 'package:review_app_mobile/screens/account.dart';
+import 'package:review_app_mobile/screens/home.dart';
+import 'package:review_app_mobile/screens/location.dart';
+import 'package:review_app_mobile/screens/new_post.dart';
+import 'package:review_app_mobile/screens/notification.dart';
+
+class TabBarNavigator extends StatefulWidget {
+  @override
+  _TabBarNavigator createState() => _TabBarNavigator();
+}
+
+class _TabBarNavigator extends State<TabBarNavigator> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> screens = [
+      Home(),
+      Location(),
+      Account(),
+      NewPost(),
+      Notifications()
+    ];
+
+    return Scaffold(
+      body: screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppTheme.blue1,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _currentIndex,
+        showUnselectedLabels: false,
+        onTap: (int index) => setState(() => _currentIndex = index),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.location_on), label: "Location"),
+          BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: "Add"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications_none), label: "Notification"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account")
+        ],
+      ),
+    );
+  }
+}
